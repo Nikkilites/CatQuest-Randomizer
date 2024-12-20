09b29707d627b5c7779293456f5b85d17c076447
Nikkilite <Niko6697n@gmail.com>
2024-12-20 19:47:43 +0100

    Change name of main file, and add Modinfo

---
+++ CatQuest-Randomizer/Randomizer.cs
@@ -0,0 +1,17 @@
+using BepInEx;
+using BepInEx.Logging;
+
+namespace CatQuest_Randomizer;
+{
+    [BepInPlugin(ModInfo.GUID, ModInfo.NAME, ModInfo.VERSION)]
+    public class Randomizer : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+            
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {ModInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
