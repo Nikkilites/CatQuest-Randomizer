9686997d2768d7642b542cbab760bbec1258713d
Nikkilite <Niko6697n@gmail.com>
2024-12-27 23:20:59 +0100

    Add UnlockablePatches with DisableObtainingArtsPatch

---
+++ CatQuest_Randomizer/Patches/UnlockablePatches.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace CatQuest_Randomizer.Patches
+{
+    [HarmonyPatch(typeof(UnlockBlock), nameof(UnlockBlock.Start))]
+    public class DisableObtainingArtsPatch
+    {
+        private static ManualLogSource Logger;
+        private static FieldInfo questIdField = typeof(UnlockBlock).GetField("questId", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        static bool Prefix(UnlockBlock __instance)
+        {
+            Logger = BepInEx.Logging.Logger.CreateLogSource("DisableObtainingArtsPatch");
+
+            switch (__instance.value)
+            {
+                case Unlockables.Flying:
+                    Logger.LogInfo($"Obtaining Flying was disabled");
+                    break;
+                case Unlockables.WaterWalking:
+                    Logger.LogInfo($"Obtaining Water Walking was disabled");
+                    break;
+                default:
+                    Game.instance.Unlock(__instance.value);
+                    break;
+            }
+
+            string questId = questIdField.GetValue(__instance) as string;
+
+            Game.gameStream.Publish(new QuestEvent
+            {
+                type = QuestEvent.EventType.QUEST_BLOCK_COMPLETE,
+                questId = questId
+            });
+
+            return false;
+        }
+    }
+}
