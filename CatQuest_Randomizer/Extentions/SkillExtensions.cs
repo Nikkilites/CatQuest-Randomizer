5e0116b1e140779c2c856a6f6aa005728f4486f7
Nikkilite <Niko6697n@gmail.com>
2024-12-27 21:05:42 +0100

    Create SkillExtensions and AddSkill method

---
+++ CatQuest_Randomizer/Extentions/SkillExtensions.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace CatQuest_Randomizer.Extentions
+{
+    public class SkillExtensions
+    {
+        public void AddSkill(string id)
+        {
+			Skill skill = Game.instance.skillManager.GetSkill(id);
+			if (!Game.instance.gameData.player.skills.obtained.Contains(skill))
+			{
+				Game.instance.skillManager.Obtain(id);
+				skill.level = 1;
+				if (!Game.instance.gameData.player.skills.equipped.Contains(skill))
+				{
+					for (int i = 0; i < 4; i++)
+					{
+						if (Game.instance.gameData.player.skills.equipped[i] == null)
+						{
+							Game.instance.skillManager.Equip(skill, i);
+							break;
+						}
+					}
+				}
+			}
+		}
+    }
+}
