093083bb2083507b6912bc38455ba8d18a422b45
Nikkilite <Niko6697n@gmail.com>
2024-12-27 21:50:06 +0100

    Make AddSkill method use Item Model

--- CatQuest_Randomizer/Extentions/SkillExtensions.cs
+++ CatQuest_Randomizer/Extentions/SkillExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using CatQuest_Randomizer.Model;
 using HarmonyLib;
 using System.Collections.Generic;
 
@@ -7,22 +8,20 @@ namespace CatQuest_Randomizer.Extentions
 {
     public class SkillExtensions
     {
-        public void AddSkill(string id)
+        public void AddSkill(Item item)
         {
-			Skill skill = Game.instance.skillManager.GetSkill(id);
-			if (!Game.instance.gameData.player.skills.obtained.Contains(skill))
+			Skill skill = Game.instance.skillManager.GetSkill(item.Id);
+			Game.instance.skillManager.Obtain(item.Id);
+			skill.level = 1;
+			CombatTextSystem.current.ShowText(CombatTextSystem.TextType.DAMAGE, $"{item.Name} obtained", Game.instance.player.GetPosition(), 1f);
+			if (!Game.instance.gameData.player.skills.equipped.Contains(skill))
 			{
-				Game.instance.skillManager.Obtain(id);
-				skill.level = 1;
-				if (!Game.instance.gameData.player.skills.equipped.Contains(skill))
+				for (int i = 0; i < 4; i++)
 				{
-					for (int i = 0; i < 4; i++)
+					if (Game.instance.gameData.player.skills.equipped[i] == null)
 					{
-						if (Game.instance.gameData.player.skills.equipped[i] == null)
-						{
-							Game.instance.skillManager.Equip(skill, i);
-							break;
-						}
+						Game.instance.skillManager.Equip(skill, i);
+						break;
 					}
 				}
 			}
