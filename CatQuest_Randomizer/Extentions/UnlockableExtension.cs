0973bc360c08a5aa016fc54a2fcd08395e20cc1e
Nikkilite <Niko6697n@gmail.com>
2024-12-30 15:28:14 +0100

    Add unlockableExtension, making it possible to receive royal arts on the fly

---
+++ CatQuest_Randomizer/Extentions/UnlockableExtension.cs
@@ -0,0 +1,16 @@
+﻿using CatQuest_Randomizer.Model;
+
+namespace CatQuest_Randomizer.Extentions
+{
+	public class UnlockableExtensions
+	{
+		public void AddRoyalArt(Item item)
+		{
+			Unlockables unlockable = (item.GetItemValue() == "flight" ? Unlockables.Flying : Unlockables.WaterWalking);
+
+			Game.instance.Unlock(unlockable);
+
+			CombatTextSystem.current.ShowText(CombatTextSystem.TextType.DAMAGE, $"{item.Name} obtained from {item.Player}", Game.instance.player.GetPosition(), 1f);
+		}
+	}
+}
