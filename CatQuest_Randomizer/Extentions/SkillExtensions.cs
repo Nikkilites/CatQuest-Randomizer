5d556e0175ea240b43193900c8ccb97bf7e786e4
Nikkilite <Niko6697n@gmail.com>
2025-01-08 22:42:33 +0100

    Fix bugs in adding skills

--- CatQuest_Randomizer/Extentions/SkillExtensions.cs
+++ CatQuest_Randomizer/Extentions/SkillExtensions.cs
@@ -6,10 +6,14 @@ namespace CatQuest_Randomizer.Extentions
     {
         public void AddSkill(Item item)
         {
-			Skill skill = Game.instance.skillManager.GetSkill(item.Id);
-			Game.instance.skillManager.Obtain(item.Id);
+			string gameSkillId = item.GetItemValue();
+
+			Randomizer.Logger.LogInfo($"Will add Skill {gameSkillId} to player");
+
+			Skill skill = Game.instance.skillManager.GetSkill(gameSkillId);
+			Game.instance.skillManager.Obtain(gameSkillId);
 			skill.level = 1;
-			CombatTextSystem.current.ShowText(CombatTextSystem.TextType.DAMAGE, $"{item.Name} obtained", Game.instance.player.GetPosition(), 1f);
+			CombatTextSystem.current.ShowText(CombatTextSystem.TextType.DAMAGE, $"{item.Name} obtained from {item.Player}", Game.instance.player.GetPosition(), 1f);
 			if (!Game.instance.gameData.player.skills.equipped.Contains(skill))
 			{
 				for (int i = 0; i < 4; i++)
