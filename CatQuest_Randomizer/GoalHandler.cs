8d39c522ce298311397f76252b837f13a9905f3e
Nikkilite <Niko6697n@gmail.com>
2025-01-08 22:42:53 +0100

    Add goal handler

---
+++ CatQuest_Randomizer/GoalHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace CatQuest_Randomizer
+{
+    public class GoalHandler
+    {
+        public void CheckIfGoal(string questId)
+        {
+            if (questId == "MainQuest_012")
+                OnGoalConditionMet();
+        }
+
+        private void OnGoalConditionMet()
+        {
+            Randomizer.ConnectionHandler.SendGoal();
+        }
+    }
+}
