970e2eca852e4d187285841cf1a781b05cce70e0
Nikkilite <Niko6697n@gmail.com>
2025-01-27 02:59:14 +0100

    Add temples as locations

---
+++ CatQuest_Randomizer/Patches/ArcaneTemplePatches.cs
@@ -0,0 +1,23 @@
+﻿using CatQuest.UI;
+using HarmonyLib;
+
+namespace CatQuest_Randomizer.Patches
+{
+    [HarmonyPatch(typeof(WorldMapHUD))]
+    [HarmonyPatch("EnterTrigger", MethodType.Normal)]
+    public class TempleCheckPatch
+    {
+        static void Postfix(WorldMapTriggerEvent e)
+        {
+            ArcaneTempleTrigger arcaneTempleTrigger = e.trigger as ArcaneTempleTrigger;
+            if (arcaneTempleTrigger != null)
+            {
+                if (arcaneTempleTrigger.skillId != "")
+                {
+                    Randomizer.Logger.LogInfo($"Temple of type {arcaneTempleTrigger.skillId} was found, send check from TempleCheckPatch");
+                    Randomizer.LocationHandler.CheckLocation(arcaneTempleTrigger.skillId);
+                }
+            }
+        }
+    }
+}
