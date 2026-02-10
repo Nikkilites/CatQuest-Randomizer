6f15654972f340c514f54a8d006b412736c30ba4
Nikkilite <Niko6697n@gmail.com>
2026-02-10 19:16:10 +0100

    rework listing all quests using catQuestDataHelper

--- CatQuest_Randomizer/Patches/QuestPatches.cs
+++ CatQuest_Randomizer/Patches/QuestPatches.cs
@@ -105,36 +105,21 @@ namespace CatQuest_Randomizer.Patches
         {
             Randomizer.Logger.LogInfo("Will list all Quests.");
 
-            var questsField = AccessTools.Field(typeof(QuestManager), "quests");
+            Dictionary<string, Quest> quests = CatQuestDataHelper.GetQuests(__instance);
 
-            if (questsField == null)
+            foreach (KeyValuePair<string, Quest> keyValuePair in quests)
             {
-                Randomizer.Logger.LogError("Could not find the 'quests' field in QuestManager.");
-                return;
-            }
+                var quest = keyValuePair.Value;
+                string questId = quest.questId;
+                LocalizedString title = quest.title;
 
-            Dictionary<string, Quest> quests = questsField.GetValue(__instance) as Dictionary<string, Quest>;
+                string prereqLevel = quest.prereq?.level.ToString() ?? "None";
 
-            if (quests != null)
-            {
-                foreach (KeyValuePair<string, Quest> keyValuePair in quests)
-                {
-                    var quest = keyValuePair.Value;
-                    string questId = quest.questId;
-                    LocalizedString title = quest.title;
+                string prereqQuests = quest.prereq?.quests != null
+                    ? string.Join(", ", quest.prereq.quests)
+                    : "None";
 
-                    string prereqLevel = quest.prereq?.level.ToString() ?? "None";
-
-                    string prereqQuests = quest.prereq?.quests != null
-                        ? string.Join(", ", quest.prereq.quests)
-                        : "None";
-
-                    Randomizer.Logger.LogInfo($"Loaded Quest: ID = {questId}, Title = {title}, Prereq Level = {prereqLevel}, Prereq Quests = {prereqQuests}");
-                }
-            }
-            else
-            {
-                Randomizer.Logger.LogError("No quests found to load.");
+                Randomizer.Logger.LogInfo($"Loaded Quest: ID = {questId}, Title = {title}, Prereq Level = {prereqLevel}, Prereq Quests = {prereqQuests}");
             }
         }
     }
