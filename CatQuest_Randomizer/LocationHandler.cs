0deeb1784a68b49ed5af265deb145e525f474668
Nikkilite <Niko6697n@gmail.com>
2026-02-10 19:20:26 +0100

    Include monument locations

--- CatQuest_Randomizer/LocationHandler.cs
+++ CatQuest_Randomizer/LocationHandler.cs
@@ -6,6 +6,38 @@ namespace CatQuest_Randomizer
 {
     public class LocationHandler
     {
+        CatQuestDataHelper _dataHelper = new();
+
+        public void Init()
+        {
+            Randomizer.Logger.LogInfo($"LocationHandler Init() Called");
+
+            Observable.EveryUpdate()
+                .Where(_ => _dataHelper.GetVisitedMonuments() != null)
+                .Take(1)
+                .Subscribe(_ =>
+                {
+                    Randomizer.Logger.LogInfo($"Setup Location Subscriptions");
+                    SetupLocationSubscriptions();
+                });
+        }
+
+        public void SetupLocationSubscriptions()
+        {
+            if (Randomizer.SlotDataHandler.includeMonuments)
+            {
+                Randomizer.Logger.LogInfo("Setup Monument subscription");
+
+                _dataHelper.GetVisitedMonuments()
+                    .ObserveAdd()
+                    .Subscribe(e =>
+                    {
+                        Randomizer.Logger.LogInfo($"Monument {e.Value.id} was visited, send check from LocationHandler");
+                        CheckLocation(e.Value.id);
+                    });
+            }
+        }
+
         public void CheckLocation(string locationId)
         {
             Randomizer.Logger.LogInfo($"Will look for Location with id {locationId} on list");
