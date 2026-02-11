242c885120e7fa2e4a5b4664790a039d92754424
Nikkilite <Niko6697n@gmail.com>
2026-02-11 18:34:16 +0100

    Make sure Locations can be loaded

--- CatQuest_Randomizer/Model/Location.cs
+++ CatQuest_Randomizer/Model/Location.cs
@@ -4,11 +4,15 @@
     {
         public string Id { get; }
         public string Name { get; }
+        public string Art {  get; }
+        public bool HasFist { get; }
 
-        public Location(string id, string name)
+        public Location(string id, string name, string art, bool hasFist)
         {
             Id = id;
             Name = name;
+            Art = art;
+            HasFist = hasFist;
         }
     }
 }
