f20b2e3e36a6022d22bff7d72cacde07219499ae
Nikkilite <Niko6697n@gmail.com>
2026-02-11 18:39:59 +0100

    Create method that checks availability of quest

--- CatQuest_Randomizer/LocationHandler.cs
+++ CatQuest_Randomizer/LocationHandler.cs
@@ -1,6 +1,10 @@
 ﻿using CatQuest_Randomizer.Model;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
 using UniRx;
+using UnityEngine;
+using static Persistent;
 
 namespace CatQuest_Randomizer
 {
@@ -55,6 +59,56 @@ namespace CatQuest_Randomizer
             {
                 Randomizer.Logger.LogError($"Location was not found on list of Locations");
             }
+        }
+
+        public bool GetLocationLogicAvailability(Quest quest)
+        {
+            Randomizer.Logger.LogInfo($"Will look for Location with id {quest.questId} on list");
+
+            Location location = Randomizer.DataStorageHandler.locations.FirstOrDefault(location => location.Id == quest.questId);
+
+            if (location == null)
+            {
+                Randomizer.Logger.LogError($"Location was not found on list of Locations");
+                return false;
+            }
+
+            Randomizer.Logger.LogInfo($"Check if {quest.questId} is in logic");
+
+            if (location.HasFist && _dataHelper.GetObtainedSkills().Count == 0)
+            {
+                return false;
+            }
+
+            var currentUnlocks = _dataHelper.GetCurrentUnlocks().Value;
+
+            bool hasWater = (currentUnlocks & Unlockables.WaterWalking) != 0;
+            bool hasFlight = (currentUnlocks & Unlockables.Flying) != 0;
+
+            switch (location.Art)
+            {
+                case "water":
+                    if (!hasWater)
+                        return false;
+                    break;
+
+                case "flight":
+                    if (!hasFlight)
+                        return false;
+                    break;
+
+                case "both":
+                    if (!(hasWater && hasFlight))
+                        return false;
+                    break;
+
+                case "either":
+                    if (!(hasWater || hasFlight))
+                        return false;
+                    break;
+            }
+
+            return true;
         }
     }
 }
