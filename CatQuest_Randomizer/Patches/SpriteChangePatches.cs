d09a2ddf4634ec69b87202b25b78d0795211fcf6
Nikkilite <Niko6697n@gmail.com>
2026-02-11 18:40:28 +0100

    Create patch that overrides Questboard Notice sprites

---
+++ CatQuest_Randomizer/Patches/SpriteChangePatches.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+using UnityEngine;
+
+namespace CatQuest_Randomizer.Patches
+{
+    public class SpriteChangePatches
+    {
+        [HarmonyPatch(typeof(QuestListItem2), nameof(QuestListItem2.SetData))]
+        public class QuestBoardSpriteOverridePatch
+        {
+            static void Postfix(Quest quest, UnityEngine.UI.Image ___notice)
+            {
+                Randomizer.Logger.LogInfo($"Override {quest.questId} notice sprite");
+
+                if (quest.isComplete)
+                {
+                    ___notice.sprite = Randomizer.DataStorageHandler.Sprites["questComplete"];
+                }
+                else if (Randomizer.LocationHandler.GetLocationLogicAvailability(quest))
+                {
+                    ___notice.sprite = Randomizer.DataStorageHandler.Sprites["questInLogic"];
+                }
+                else
+                {
+                    ___notice.sprite = Randomizer.DataStorageHandler.Sprites["questOutOfLogic"];
+                }
+            }
+        }
+    }
+}
